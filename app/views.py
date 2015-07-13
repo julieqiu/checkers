2950f1e82c4bf3baacb48d919ccd3012315d5a94
Julie Qiu <julieyeqiu@gmail.com>
2015-07-13 12:31:07 -0400

    2 Player games, multiple sessions

--- app/views.py
+++ app/views.py
@@ -3,23 +3,37 @@ from app import app
 import checkers
 
 user = {'nickname': 'Miguel'}  # fake user
-x = checkers.Checkers("1", "2")
 moves_lst = ["0", "1", "2", "3", "4", "5", "6", "7"]
 player_lst = ["R", "B"]
+game_id = 0
+games = {}
+player_id_1 = "R"
+player_id_2 = "B"
 player_must_jump = False
 player_must_move = None
 
 
 @app.route('/')
-@app.route('/index')
-def index():
+def start():
+    global game_id
+    game_id += 1
+
+    games[game_id] = checkers.Checkers(player_id_1, player_id_2)
+
+    resp = render_template('start.html',
+            title='Checkers',
+            game_id=game_id,
+            player_id_1=player_id_1,
+            player_id_2=player_id_2)
+    return resp
+
+@app.route('/index/<game_id>/<player_id>')
+def index(game_id, player_id):
 
     global player_must_move
     global player_must_jump
 
-    print "USER"
-    user = request.cookies.get("user")
-    print user
+    x = games[int(game_id)]
 
     try:
         row = request.args.get("row")
@@ -36,7 +50,9 @@ def index():
             row = player_must_move[0]
             col = player_must_move[1]
 
-        if not x.client_check_valid_piece(row, col):
+        if x.current_player.color != player_id:
+            raise "It's not your turn"
+        elif not x.client_check_valid_piece(row, col):
             raise "Invalid Piece Selected"
         elif x.board.is_valid_move(row, col, to_row, to_col) and not \
         player_must_jump:
@@ -65,7 +81,7 @@ def index():
     print board
 
     resp = make_response(
-            render_template('index.html',
+           render_template('index.html',
                            title='Checkers',
                            board=board,
                            user=user,
@@ -74,17 +90,10 @@ def index():
                            to_row=to_row,
                            to_col=to_col,
                            moves_lst=moves_lst,
+                           game_id=game_id,
+                           player_id=player_id,
                            current_player=current_player))
     return resp
-
-
-@app.route('/set_user')
-def set_user():
-    user = request.args.get("user")
-    resp = index()
-    resp.set_cookie("user", user)
-    return resp
-
 
 @app.route('/move/<row>/<col>/<to_row>/<to_col>')
 def move_piece(row, col, to_row, to_col):
