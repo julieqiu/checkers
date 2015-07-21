bf71c164af967136d6483428e9be36d25cb53688
Julie Qiu <julieyeqiu@gmail.com>
2015-07-21 18:01:09 -0400

    heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,12 @@
 from flask import Flask
 import sys
-from logging import Formatter
+import logging
 
 app = Flask(__name__)
 
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
-
-def log_to_stderr(app):
-    handler = logging.StreamHandler(sys.stderr)
-    handler.setFormatter(Formatter(
-        '%(asctime)s %(levelname)s: %(message)s '
-        '[in %(pathname)s:%(lineno)d]'
-    ))
-    handler.setLevel(logging.WARNING)
-    app.logger.addHandler(handler)
-
-
 
 
 from app import views, models
