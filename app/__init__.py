1242a687b9b523997b684969a153539d0f53f1e2
Julie Qiu <julieyeqiu@gmail.com>
2015-07-21 17:52:56 -0400

    logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,23 @@
 from flask import Flask
 import sys
-import logging
+from logging import Formatter
 
 app = Flask(__name__)
 
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
+
+def log_to_stderr(app):
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setFormatter(Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]'
+    ))
+    handler.setLevel(logging.WARNING)
+    app.logger.addHandler(handler)
+
+
 
 
 from app import views, models
